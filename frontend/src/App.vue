e1c232f79057c6ba6bef65f511cfe3c9b67f00ed
JC BUISSON <buisson@enseeiht.fr>
2026-06-02 08:00:14 +0200

    WIP

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@
 
       <textarea
          v-model="frenchText"
-         placeholder="1Entrez une phrase en français…"
+         placeholder="2Entrez une phrase en français…"
          rows="4"
          :disabled="loading"
       />
